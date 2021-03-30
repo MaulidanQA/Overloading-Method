27b3e334d5bf42de4a5488a12478dc6c67412997
maulidan <maulidanqa666@gmail.com>
2021-03-30 23:00:38 +0700

    Tugas pertemuan ke-5, Nilai Max and Nilai Min

---
+++ LatihanOverloading/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace LatihanOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat object dari class numbers
+            Numbers number = new Numbers();
+
+            Console.WriteLine("Nilai Minimum #1 : {0}", number.FindMinimum(3, 1));
+            Console.WriteLine("Nilai Minimum #2 : {0}", number.FindMinimum(3, 2, 4));
+            Console.WriteLine();
+            Console.WriteLine("Nilai Maximum #1 : {0}", number.FindMaximum(3, 1));
+            Console.WriteLine("Nilai Maximum #2 : {0}", number.FindMaximum(3, 2, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
